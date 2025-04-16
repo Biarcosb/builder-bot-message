d55c820872def44276e2044a8c6aab123b174d60
Biarcosb <biarcosb@uqvirtual.edu.co>
2025-04-16 16:01:56 -0500

    budget_card

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,9 @@ import { join } from 'path'
 import { createBot, createProvider, createFlow, addKeyword, utils } from '@builderbot/bot'
 import { MemoryDB as Database } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
-
 const PORT =  3008
-
-
 const fullSamplesFlow = addKeyword<Provider, Database>(['samples', utils.setEvent('SAMPLES')])
     .addAnswer('💪')
-    
 
 const main = async () => {
     const adapterFlow = createFlow([fullSamplesFlow])
@@ -17,9 +13,7 @@ const main = async () => {
         experimentalStore: true,  // Significantly reduces resource consumption
         timeRelease: 10800000,    // Cleans up data every 3 hours (in milliseconds)
     })
-
     const adapterDB = new Database()
-
     const { handleCtx, httpServer } = await createBot({
         flow: adapterFlow,
         provider: adapterProvider,
